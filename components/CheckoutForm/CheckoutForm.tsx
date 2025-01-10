e6b9393bb1747b637e707b8f1e2e7450e2158e2a
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2025-01-10 19:26:53 +0530

    Update CheckoutForm.tsx

--- components/CheckoutForm/CheckoutForm.tsx
+++ components/CheckoutForm/CheckoutForm.tsx
@@ -31,7 +31,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/payment",
+        return_url: "https://doctor-dashboard-app-frontend.vercel.app/payment",
       },
     });
 
